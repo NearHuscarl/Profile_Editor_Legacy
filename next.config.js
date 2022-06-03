fd47c5dc843a8734e00da44516185aa8f7823196
Near Huscarl <near.huscarl@gmail.com>
2022-06-04 00:01:14 +0700

    redirect to the new domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "https://superfighters.vercel.app/",
+        permanent: true,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
